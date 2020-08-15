211c29199100378e4d43e0cd3d8642be07782366
A113837 <padi196@live.com>
2020-08-15 19:29:01 +0800

    Add files via upload

---
+++ 06 04 Why autowiring in spring | XML | @Autowired | @qualifier | spring annotation tutorial for beginners-autowired at constructor/src/com/seleniumexpress/autowired/Human.java
@@ -0,0 +1,36 @@
+package com.seleniumexpress.autowired;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Human {
+	
+	private Heart heart;
+	
+	public Human() {
+		
+	}
+
+	@Autowired
+	public Human(Heart heart) {
+		this.heart = heart;
+		System.out.println("human constr is get called");
+	}
+
+	public void setHeart(Heart heart) {
+		this.heart = heart;
+		System.out.println("setter method heart called");
+	}
+	
+	public void startPumping() {
+		if( heart != null)
+		{
+			heart.pump();
+		}
+		else
+		{
+			System.out.println("You are dead");
+		}
+		
+	}
+
+}
