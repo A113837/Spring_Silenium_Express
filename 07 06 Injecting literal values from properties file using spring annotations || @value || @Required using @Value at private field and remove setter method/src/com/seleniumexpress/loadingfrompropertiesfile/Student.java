8dbb10a82e90724d3d441c07ccf3ea7cbc182a8d
A113837 <padi196@live.com>
2020-08-16 16:27:21 +0800

    Add files via upload

---
+++ 07 06 Injecting literal values from properties file using spring annotations || @value || @Required using @Value at private field and remove setter method/src/com/seleniumexpress/loadingfrompropertiesfile/Student.java
@@ -0,0 +1,28 @@
+package com.seleniumexpress.loadingfrompropertiesfile;
+
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Value;
+
+public class Student {
+
+	@Value("${student.name}")
+	private String name;
+	
+	@Value("${student.interestedCourse}")
+	private String interestedCourse;
+	
+	@Value("${student.hobby}")
+	private String hobby;
+	
+	
+
+	
+	public void displayStudentInfo() {
+		 
+		System.out.println("Student name: "+ name);
+		System.out.println("Student interested course: "+ interestedCourse);
+		System.out.println("Student hobby: "+ hobby);
+	}
+	
+	
+}
