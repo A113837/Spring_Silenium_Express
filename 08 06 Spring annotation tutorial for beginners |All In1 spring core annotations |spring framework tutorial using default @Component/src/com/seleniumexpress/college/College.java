ef12b008c3b0e23fdb9ed0ea3c7cf51760090712
A113837 <padi196@live.com>
2020-08-17 00:21:11 +0800

    Add files via upload

---
+++ 08 06 Spring annotation tutorial for beginners |All In1 spring core annotations |spring framework tutorial using default @Component/src/com/seleniumexpress/college/College.java
@@ -0,0 +1,12 @@
+package com.seleniumexpress.college;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class College {
+	
+	public void test() {
+		System.out.println("Testing this class College college methods");
+	}
+
+} 
---
+++ 08 06 Spring annotation tutorial for beginners |All In1 spring core annotations |spring framework tutorial using default @Component/src/com/seleniumexpress/college/CollegeConfig.java
@@ -0,0 +1,20 @@
+package com.seleniumexpress.college;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.seleniumexpress.college")
+public class CollegeConfig {
+
+//	@Bean
+//	public College collegeBean() //collegeBean - bean id
+//	{
+//		
+//		College college = new College();
+//		return college;
+//	}
+	
+	
+}
