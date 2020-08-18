160c545a1c375ebfe647410904d1172742f13b49
A113837 <padi196@live.com>
2020-08-18 16:56:58 +0800

    Add files via upload

---
+++ 08 16 Spring annotation tutorial for beginners |All In1 spring core annotations |spring framework tutorial create bean from CollegeConfi using setter and interface with @Autowired without setter method + @PropertySource +@Qalifier(multiple bean)/src/com/seleniumexpress/college/College.java
@@ -0,0 +1,34 @@
+package com.seleniumexpress.college;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class College {
+	
+	@Value("${college.Name}")
+	private String collegeName;
+	
+	@Autowired	
+	private Principal principal;
+	
+	@Autowired
+	@Qualifier("mathTeacher")
+	private Teacher teacher;
+	
+	
+
+	public void test() {
+		System.out.println("invoke College class @Bean CollegeConfig testId method");
+		teacher.teach();
+		System.out.println("My college name is: " + collegeName);
+		principal.principalInfo();
+	}
+
+
+
+
+
+}
