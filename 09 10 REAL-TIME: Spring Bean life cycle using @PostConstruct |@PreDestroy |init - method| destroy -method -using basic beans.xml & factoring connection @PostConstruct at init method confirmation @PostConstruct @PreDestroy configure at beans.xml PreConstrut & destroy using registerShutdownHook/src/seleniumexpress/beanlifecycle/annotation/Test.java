567de8a637da1b05e8d2c4b64222291fb6a040ae
A113837 <padi196@live.com>
2020-08-26 14:45:36 +0800

    Add files via upload

---
+++ 09 10 REAL-TIME: Spring Bean life cycle using @PostConstruct |@PreDestroy |init - method| destroy -method -using basic beans.xml & factoring connection @PostConstruct at init method confirmation @PostConstruct @PreDestroy configure at beans.xml PreConstrut & destroy using registerShutdownHook/src/seleniumexpress/beanlifecycle/annotation/Test.java
@@ -0,0 +1,25 @@
+package seleniumexpress.beanlifecycle.annotation;
+
+import java.sql.SQLException;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException  {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		
+		StudentDAO studentDAO = context.getBean("studentDAO", StudentDAO.class);
+		System.out.println(studentDAO);
+		
+		studentDAO.selectAllRows();
+		
+		((ClassPathXmlApplicationContext) context).registerShutdownHook();
+		
+		//((ClassPathXmlApplicationContext) context).close();
+	}
+
+}
