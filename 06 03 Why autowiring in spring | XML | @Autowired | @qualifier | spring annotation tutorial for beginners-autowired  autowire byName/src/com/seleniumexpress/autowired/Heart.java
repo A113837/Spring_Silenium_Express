52cde1db32bef151452c7d423383cc4c6fa5a431
A113837 <padi196@live.com>
2020-08-26 16:54:00 +0800

    Add files via upload

---
+++ 06 03 Why autowiring in spring | XML | @Autowired | @qualifier | spring annotation tutorial for beginners-autowired  autowire byName/src/com/seleniumexpress/autowired/Heart.java
@@ -0,0 +1,12 @@
+package com.seleniumexpress.autowired;
+
+
+public class Heart {
+	
+	public void pump()
+	{
+		System.out.println("your heart is pumping");
+		System.out.println("Alive");
+	}
+
+}
